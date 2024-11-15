c6aa2803b1fb9d693fe069d897f9c1ad5b190e13
Bilal Ahmad <bilalmomand709@gmail.com>
2024-11-15 13:08:24 +0500

    change img source using js

---
+++ change-attribute/change-attribute.js
@@ -0,0 +1,10 @@
+let onbulb= document.getElementById("bulb");
+onbulb.src='/change-attribute/images/bulbon.webp';
+
+function bulbOnn(){
+    document.getElementById("bulb").src='/change-attribute/images/bulbon.webp';
+}
+
+function bulbOff(){
+    document.getElementById("bulb").src='/change-attribute/images/bulboff.jpeg';
+}
