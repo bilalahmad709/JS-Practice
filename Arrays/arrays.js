ac91d2c3acbfc2e727b01fb0111e7b042ba9cf5e
Bilal Ahmad <bilalmomand709@gmail.com>
2024-11-19 14:04:27 +0500

    added arrays

---
+++ Arrays/arrays.js
@@ -0,0 +1,8 @@
+let myArray =[10,20,30,40,50,60,70,80,90,100];
+
+document.write(myArray);
+
+for(k=0;k<myArray.length;k++){
+    document.write(myArray[k] +"<br>");
+}
+document.write( "The addition of values of index 2 and index 4 is :" +(myArray[2]+myArray[4]));
---
+++ js events/Select_submit_events/select_submit_events.js
@@ -0,0 +1,11 @@
+function onselectFunction(){
+    alert("you selected the text");
+}
+
+function submitFunction(){
+    alert("Your form has been submited successfully");
+}
+
+function invalidfunction(){
+    alert("Please fill the field first and then submit");
+}
