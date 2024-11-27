0447c17d130d3087c205344ed8be65a520fb27d3
Bilal Ahmad <bilalmomand709@gmail.com>
2024-11-27 14:22:54 +0500

    added to tic tac toe

---
+++ Objects and Classes/objectClass.js
@@ -0,0 +1,17 @@
+// The below is a class
+class hello{
+    message(){
+        document.write("I am Learning OOPs");
+    }
+
+    sorry(){
+        document.write("Sorry OOps is very dangrous");
+    }
+}
+
+// if I want to call it , then i have to make an object of this class
+// whenever i want to call the class, then i have to use object name.
+
+let classObject = new hello();
+classObject.message();
+classObject.sorry();
--- Tic tac toe/ticTac.js
+++ Tic tac toe/ticTac.js
@@ -1,7 +1,10 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#rest_button");
+let newGame = document.querySelector(".new-Game");
+let winnerDiv = document.querySelector(".winner-div");
+let winnerP = document.querySelector(".winner-p");
 
-let turn0 = true;  //playerx , player0;
+let turnA = true;  //playerA , playerB;
 
 const winpatterns= [
     [0, 1, 2],
@@ -16,6 +19,38 @@ const winpatterns= [
 
 boxes.forEach((box) =>{
    box.addEventListener("click", () =>{
-    console.log("you click it");
+    if(turnA){
+        box.innerText = "A";
+        box.style.color = "red";
+        turnA = false;
+    }
+    else{
+        box.innerText = "B";
+        turnA = true;
+    }
+    box.disabled = true;
+    checkWinner();
    });
 });
+
+const showWinner = (winner) =>{
+    winnerP.innerText = `The Winner of the Game is ${winner}`;
+    winnerDiv.classList.remove("hide_winner");
+}
+
+// function for checking patterens
+const checkWinner = () =>{
+    for (let pattern of winpatterns){
+        let position1val = boxes[pattern[0]].innerText;
+        let position1va2 = boxes[pattern[1]].innerText;
+        let position1va3 = boxes[pattern[2]].innerText;
+
+        if (position1val != "" && position1va2!= "" && position1va3 != ""){
+            if(position1val === position1va2 && position1va2 === position1va3){
+            console.log ("winner is ", position1val);
+            showWinner(position1val);
+            }
+        };
+
+    }
+}
