87b22324f0d27773e2ef930b3497e85de132f93e
Bilal Ahmad <bilalmomand709@gmail.com>
2024-11-27 17:50:13 +0500

    added to class and object files

--- Objects and Classes/objectClass.js
+++ Objects and Classes/objectClass.js
@@ -1,3 +1,9 @@
+//IMPORTANT NOTE
+
+// the class keyword is not strictly necessary to define a class in JavaScript. Before ES6 introduced the class keyword, JavaScript used functions to create objects and simulate classes. This approach is still valid and widely used in some cases.
+
+//
+
 // The below is a class
 class hello{
     message(){
@@ -5,7 +11,8 @@ class hello{
     }
 
     sorry(){
-        document.write("Sorry OOps is very dangrous");
+        document.write("<br> Sorry OOps is very dangrous");
+        console.log("hello")
     }
 }
 
@@ -15,3 +22,43 @@ class hello{
 let classObject = new hello();
 classObject.message();
 classObject.sorry();
+
+//second example of class
+class car{
+    constructor(brand, color){
+        this.brand = brand;
+        this.color = color;
+    }
+    drive(){
+        document.write(`<br>${this.color} ${this.brand} is driving`);
+    }
+    sold(){
+        document.write(`<br> <br>The ${this.color} Jeep sold to someone. beacause he likes ${this.brand} company's car`);
+    }
+}
+
+const myCar = new car("Mehran", "white");
+myCar.drive();
+const soldCar = new car ("TOYOTA" , "Black");
+soldCar.sold();
+
+//third example of the class
+class calculation{
+     constructor(){
+        this.a = 10;
+        this.b = 20;
+     }
+     add(){
+        let c = this.a + this.b;
+        return c;
+     }
+     sub(){
+        let c = this.b - this.a;
+        return c;
+     }
+}
+const addition = new calculation();
+document.write("<br><br> The addition of 'A' and 'B' is : " + addition.add());
+
+const subtraction = new calculation();
+document.write("<br><br> The addition of 'A' and 'B' is : " + subtraction.sub());
