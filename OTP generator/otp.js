424394f3abad8c18313e08fc0945a6407da5496b
Bilal Ahmad <bilalmomand709@gmail.com>
2024-12-24 20:54:47 +0500

    added otp generator

---
+++ OTP generator/otp.js
@@ -0,0 +1,16 @@
+// generateotp=()=>{
+//     // let digits= "0123456789";
+//     let otp = "";
+//     for(let i=0; i<5; i++){
+//         otp += digits(Math.floor(Math.random() *10));
+//     }
+//     document.write(otp);
+// }
+function generateotp(number){
+    let otp ='';
+    for(let i=0; i<number;i++){
+        otp += Math.trunc(Math.random()*10).toString()
+    }
+    return otp;
+}
+document.write(generateotp(4));
